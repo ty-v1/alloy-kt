a1d2dc13e97c429fe13b66ad27712f418185b721
ty-v1 <23548312+ty-v1@users.noreply.github.com>
2026-03-20 16:46:30 +0900

    setup project

---
+++ eslint.config.ts
@@ -0,0 +1,40 @@
+import { configs as jsConfigs } from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { flatConfigs as importConfigs } from "eslint-plugin-import";
+import { configs as tsConfigs } from "typescript-eslint";
+
+export default defineConfig([
+  {
+    ignores: ["**/dist/"],
+  },
+  jsConfigs.recommended,
+  tsConfigs.recommendedTypeChecked,
+  importConfigs.recommended,
+  importConfigs.typescript,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.ts", "vitest.config.ts"],
+        },
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          alphabetize: { order: "asc" },
+        },
+      ],
+      "no-unused-vars": "off",
+      "prefer-arrow-callback": "error",
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+  },
+  eslintConfigPrettier,
+]);
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import alloyPlugin from "@alloy-js/rollup-plugin";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["test/**/*.test.ts", "test/**/*.test.tsx"],
+    exclude: ["test/**/*.d.ts"],
+  },
+  esbuild: {
+    // @ts-expect-error it seems to need
+    jsx: "preserve",
+    sourcemap: "both",
+  },
+  plugins: [alloyPlugin()],
+});
