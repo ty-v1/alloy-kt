8b85f1ebdfd427a488dbd96e7acf44e844aa7af9
ty-v1 <23548312+ty-v1@users.noreply.github.com>
2026-05-24 21:56:23 +0900

    add class component

---
+++ src/component/SupertypeList.tsx
@@ -0,0 +1,34 @@
+import { Children, For } from "@alloy-js/core";
+import { isEmptyish, isNonNullish } from "remeda";
+
+type SupertypeListProps = {
+  readonly extends?: Children;
+  readonly implements?: Children[];
+};
+
+/**
+ * Kotlin supertype list, e.g. `: Parent, Interface1, Interface2`.
+ */
+export const SupertypeList = (props: SupertypeListProps) => {
+  const all: Children[] = [];
+  if (isNonNullish(props.extends)) {
+    all.push(props.extends);
+  }
+
+  if (props.implements) {
+    all.push(...props.implements);
+  }
+
+  if (isEmptyish(all)) {
+    return "";
+  }
+
+  return (
+    <>
+      {" : "}
+      <For each={all} joiner=", ">
+        {(e) => e}
+      </For>
+    </>
+  );
+};
