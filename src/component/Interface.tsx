807c8ee1e9386dbee4c6c8755090188e54414ec5
ty-v1 <23548312+ty-v1@users.noreply.github.com>
2026-05-24 21:56:44 +0900

    add interface component

---
+++ src/component/Interface.tsx
@@ -0,0 +1,47 @@
+import { Block, Children, Namekey, Refkey, Show } from "@alloy-js/core";
+import { Name } from "@alloy-js/java";
+import { isNullish } from "remeda";
+import { Declaration } from "./Declaration.js";
+import { LexicalScope } from "./LexicalScope.js";
+import { Modifiers } from "./Modifiers.js";
+import { SupertypeList } from "./SupertypeList.js";
+import { TypeParameters, TypeParametersProps } from "./TypeParameters.js";
+
+export type InterfaceProps = TypeParametersProps & {
+  readonly name: string | Namekey;
+  readonly refkey?: Refkey;
+  readonly children?: Children;
+  // visibility
+  readonly public?: boolean;
+  readonly private?: boolean;
+  readonly protected?: boolean;
+  readonly internal?: boolean;
+  // interface-specific
+  readonly sealed?: boolean;
+  /**
+   * Functional interface.
+   */
+  readonly fun?: boolean;
+  readonly extends?: Children[];
+};
+
+/**
+ * Kotlin `interface` declaration.
+ */
+export const Interface = (props: InterfaceProps) => {
+  return (
+    <Declaration {...props} name={props.name} nameKind="interface">
+      <group>
+        <Modifiers {...props} />
+        {props.fun ? "fun " : ""}interface <Name />
+        <Show when={!isNullish(props.generics)}>
+          <TypeParameters generics={props.generics} />
+        </Show>
+        <SupertypeList implements={props.extends} />{" "}
+        <LexicalScope>
+          <Block>{props.children}</Block>
+        </LexicalScope>
+      </group>
+    </Declaration>
+  );
+};
