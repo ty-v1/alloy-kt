40e0d8fa0bae4c6a6e1e6afd2f4860881764ab82
ty-v1 <23548312+ty-v1@users.noreply.github.com>
2026-07-04 12:38:43 +0900

    format code

--- test/component/Arguments.test.tsx
+++ test/component/Arguments.test.tsx
@@ -22,9 +22,7 @@ describe("Arguments", () => {
   });
 
   it("omits nullable elements", () => {
-    const res = renderToString(
-      <Arguments args={[{ value: "a" }, null, undefined, { value: "b" }]} />,
-    );
+    const res = renderToString(<Arguments args={[{ value: "a" }, null, undefined, { value: "b" }]} />);
 
     expect(res).toBe("(a, b)");
   });
@@ -51,17 +49,13 @@ describe("Named arguments", () => {
   });
 
   it("renders mixed positional and named arguments", () => {
-    const res = renderToString(
-      <Arguments args={[{ value: "pos" }, { name: "x", value: "1" }]} />,
-    );
+    const res = renderToString(<Arguments args={[{ value: "pos" }, { name: "x", value: "1" }]} />);
 
     expect(res).toBe("(pos, x = 1)");
   });
 
   it("omits null elements when mixed with named arguments", () => {
-    const res = renderToString(
-      <Arguments args={[null, { name: "x", value: "1" }, undefined]} />,
-    );
+    const res = renderToString(<Arguments args={[null, { name: "x", value: "1" }, undefined]} />);
 
     expect(res).toBe("(x = 1)");
   });
@@ -88,7 +82,12 @@ describe("Spread arguments", () => {
 
   it("renders mix of spread and named arguments", () => {
     const res = renderToString(
-      <Arguments args={[{ value: "arr", spread: true }, { name: "x", value: "1" }]} />,
+      <Arguments
+        args={[
+          { value: "arr", spread: true },
+          { name: "x", value: "1" },
+        ]}
+      />,
     );
 
     expect(res).toBe("(*arr, x = 1)");
@@ -96,7 +95,12 @@ describe("Spread arguments", () => {
 
   it("renders multiple spread arguments", () => {
     const res = renderToString(
-      <Arguments args={[{ value: "arr1", spread: true }, { value: "arr2", spread: true }]} />,
+      <Arguments
+        args={[
+          { value: "arr1", spread: true },
+          { value: "arr2", spread: true },
+        ]}
+      />,
     );
 
     expect(res).toBe("(*arr1, *arr2)");
